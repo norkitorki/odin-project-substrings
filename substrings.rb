fe8ebdfada86fc0167c03cd2b838c928fa70afca
norkitorki <norkitorki@tutanota.de>
2021-02-06 11:19:54 +0100

    add basic exception handling

--- substrings.rb
+++ substrings.rb
@@ -1,4 +1,5 @@
 def substrings(str, arr)
+  raise ArgumentError unless str.is_a?(String) && arr.is_a?(Array)
   str.split(/[^\w]/).reduce(Hash.new(0)) do |hash, word|
     arr.each { |sub_word| hash[sub_word] += 1 if word.match(/#{sub_word}/i) }
     hash
